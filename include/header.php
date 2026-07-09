9794aa2091786abac676a7745a43b1380147360b
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-09 23:36:06 +0800

    shortcut for head

--- include/header.php
+++ include/header.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Chatbot UI with Sidebar</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    
-</body>
-</html>
