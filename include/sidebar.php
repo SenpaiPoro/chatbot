be9aae26aaf7e2d4fd948b63e6d0c15b4afc049b
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-09 23:36:36 +0800

    include header for these files

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php include ('include/header.php'); ?>
 <body>
-    
-</body>
-</html>
+  <div class="sidebar">
+    <h2>Menu</h2>
+    <a href="#">Dashboard</a>
+    <a href="profile.php">Profile</a>
+    <a href="#">Settings</a>
+    <a href="#">Logout</a>
+  </div>
