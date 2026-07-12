5b412d501dc7af604e7577deffb3ab65748e4003
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-12 22:04:48 +0800

    link the index to the sidebar and change the alignment of the pages

--- include/sidebar.php
+++ include/sidebar.php
@@ -2,8 +2,8 @@
   <!-- Sidebar -->
   <div class="sidebar">
     <h2>Menu</h2>
-    <a href="#">Dashboard</a>
     <a href="profile.php">Profile</a>
+    <a href="index.php">Chat</a>
     <a href="#">Settings</a>
     <a href="#">Logout</a>
   </div>
