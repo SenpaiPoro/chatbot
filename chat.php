c82a121e996662a4876246d76b7273ac2730ac8c
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-12 23:36:07 +0800

    add hard coded return data tot he classess

---
+++ chat.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/classes/Chatbot.php';
+
+header('Content-Type: application/json');
+
+if (isset($_POST['message'])) {
+    $bot = new Chatbot();
+    $reply = $bot->getResponse($_POST['message']);
+    echo json_encode(["reply" => $reply]);
+}
--- classes/chatbot.php
+++ classes/chatbot.php
@@ -1,4 +1,18 @@
 <?php
+class Chatbot {
+    private $defaultResponse = "Sorry, I don’t understand. Please contact support.";
 
+    public function getResponse(string $message): string {
+        $msg = strtolower(trim($message));
 
-?>
+        if (strpos($msg, "password") !== false) {
+            return "You can reset your password at: /reset-password";
+        } elseif (strpos($msg, "order") !== false) {
+            return "Track your order here: /track-order";
+        } elseif (strpos($msg, "refund") !== false) {
+            return "Refunds are processed within 5 business days. Need help? Call us at +1800123456.";
+        }
+
+        return $this->defaultResponse;
+    }
+}
