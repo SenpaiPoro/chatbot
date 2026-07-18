5525a4f38024d15627d3ec7a7d2e3b7b02cd3a78
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-19 03:35:25 +0800

    trying to create  a reusable function:

--- classes/chatbot.php
+++ classes/chatbot.php
@@ -1,16 +1,26 @@
 <?php
-class chatbot {
-    private $defaultResponse = "Sorry, I don’t understand. Please contact support.";
+class Chatbot {
+    private $defaultResponse;
+
+    public function __construct($defaultResponse = "Sorry, I don’t understand. Please contact support.") {
+        $this->defaultResponse = $defaultResponse;
+    }
+
     public function getResponse(string $message): string {
         $msg = strtolower(trim($message));
 
-        if (strpos($msg, "password") !== false) {
-            return "You can reset your password at: /reset-password";
-        } elseif (strpos($msg, "order") !== false) {
-            return "Track your order here: /track-order";
-        } elseif (strpos($msg, "refund") !== false) {
-            return "Refunds are processed within 5 business days. Need help? Call us at +1800123456.";
+        $responses = [
+            "password" => "You can reset your password at: /reset-password",
+            "order"    => "Track your order here: /track-order",
+            "refund"   => "Refunds are processed within 5 business days. Need help? Call us at +1800123456."
+        ];
+
+        foreach ($responses as $keyword => $reply) {
+            if (strpos($msg, $keyword) !== false) {
+                return $reply;
+            }
         }
+
         return $this->defaultResponse;
     }
 }
