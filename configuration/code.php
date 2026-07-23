365a71fca36f4812dd2efc65423e7b8986c56c10
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-23 23:15:38 +0800

    creeate a code file whhere buttons will be processing

---
+++ configuration/code.php
@@ -0,0 +1,17 @@
+<?php
+include 'connection.php';
+
+if(isset($_POST['add'])){
+    $keyword = trim($_POST['keyword']);
+    $reply   = trim($_POST['reply']);
+
+    if($keyword != '' && $reply != ''){
+        $stmt = $conn->prepare("INSERT INTO chatbot_responses (keyword, reply) VALUES (?, ?)");
+        $stmt->bind_param("ss", $keyword, $reply);
+        $stmt->execute();
+    }
+}
+
+$responses = $conn->query("SELECT * FROM chatbot_responses");
+
+?>
