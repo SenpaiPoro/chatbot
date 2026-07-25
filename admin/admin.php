e097bb1081d28f746750be6c7087bd6f63ac981d
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-26 00:13:47 +0800

    still debugging

--- admin/admin.php
+++ admin/admin.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__  . '../classes/chatbot.php';   
+require_once __DIR__  . '../classes/chatbot.php';  
 include '../configuration/connection.php'; // your DB connection
 
 // Handle form submission
