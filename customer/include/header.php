0b09330e5a6a9f65edc89ed2353e47f571629520
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-07-31 02:02:27 +0800

    change the drop down option

--- customer/include/header.php
+++ customer/include/header.php
@@ -22,14 +22,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Hotels</a></li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Help</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><ga class="dropdown-item" href="#!">All Products</ga></li>
+                                <li><a class="dropdown-item" href="#!">Chat Support</a></li>
                                 <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                                <li><a class="dropdown-item" href="#!">Sign Up</a></li>
+                                <li><a class="dropdown-item" href="#!">About</a></li>
                             </ul>
                         </li>
                     </ul>
