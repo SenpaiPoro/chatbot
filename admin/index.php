c4fe68615eed812c204a16a1838e0c8d662ac72c
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-02 21:43:24 +0800

    adjust the link

--- admin/index.php
+++ admin/index.php
@@ -23,9 +23,9 @@
                             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
                                 <h2 class="fs-4 fw-bold">Chat Control</h2>
+                                </a>
                                 <p class="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
                             </div>
-                            </a>
                         </div>
                     </div>
                     <div class="col-lg-6 col-xxl-4 mb-5">
