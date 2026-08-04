dd4c7af807335695be7c2a3b442a57336ecf4898
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-05 05:42:55 +0800

    remove the header

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,7 @@
 <?php include 'include/header.php'; ?>
-        <!-- Header-->
-        <header class="py-5">
-            <div class="container px-lg-5">
-                <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
-                    <div class="m-4 m-lg-5">
-                        <h1 class="display-5 fw-bold">A warm welcome!</h1>
-                        <p class="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
-                        <a class="btn btn-primary btn-lg" href="#!">Call to action</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-
         <!-- Page Content-->
-        <section class="pt-4">
-            <div class="container px-lg-5">
+        <section class="pt-6 py-5 px-lg-5">
+            <div class="container px-lg-7">
                 <!-- Page Features-->
                 <div class="row gx-lg-5">
                     <div class="col-lg-6 col-xxl-4 mb-5">
