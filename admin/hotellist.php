f56c32189d7da5f563818750d16b4d50959deccd
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-06 23:57:16 +0800

    create new file for list of the hotels branch and will link it from the index

---
+++ admin/hotellist.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+
+
+        <!-- Start of  Content -->
+        <section class="py-5">
+            <div class="container px-5 my-5">
+                <div class="row gx-5">
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="card bg-light border-0 h-100">
+                    </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
+                                <h2 class="fs-4 fw-bold">Hotel Management</h2>
+                                <p class="mb-0">Manage your hotel's operations efficiently with our comprehensive management tools.</p>
+                            </div>
+                        </div>
+                        
+<?php include 'footer.php'; ?>
--- admin/index.php
+++ admin/index.php
@@ -16,10 +16,10 @@
                         </div>
                     </div>
                     <div class="col-lg-6 col-xxl-4 mb-5">
+                    <a accesskey="" href="hotellist.php" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
                         <div class="card bg-light border-0 h-100">
-                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
-                                <h2 class="fs-4 fw-bold">Hotel Management</h2>
+                                <h2 class="fs-4 fw-bold">Hotel Management</h2></a>
                                 <p class="mb-0">Manage your hotel's operations efficiently with our comprehensive management tools.</p>
                             </div>
                         </div>
