6d3abd82874905ded1950b5dde5e745ba679789d
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-09 21:49:57 +0800

    add new file for form adding the hotel

---
+++ admin/addhotel.php
@@ -0,0 +1,30 @@
+<?php include 'include/header.php'; ?>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="mb-0"><strong>Add Hotel</strong></h4>
+            </div>
+            <div class="card-body">
+                <form action="include/addhotel.php" method="POST">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="address">Address</label>
+                        <input type="text" class="form-control" id="address" name="address" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="code">Code</label>
+                        <input type="text" class="form-control" id="code" name="code" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add Hotel</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'include/footer.php'; ?>
