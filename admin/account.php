6a07101387405fea9b23c44422685b982a799ec1
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-11 06:21:34 +0800

    init for the account file

--- admin/account.php
+++ admin/account.php
@@ -1,2 +1,10 @@
 <?php include 'include/header.php'; ?>
+        <!-- Page Content-->
+        <section class="pt-6 py-5 px-lg-5">
+            <div class="container px-lg-7">
+                <h1 class="mb-4">Account Control</h1>
+                <p class="lead">Manage user accounts and permissions with ease.</p>
+            </div>
+        </section>
+        <!-- End of Page Content -->
 <?php include 'include/footer.php'; ?>
