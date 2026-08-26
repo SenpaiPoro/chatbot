931e24ed7ec0cbe3ab81b54b8e38eaa2cb329095
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-27 06:10:14 +0800

    fix the UI, and will display the data of the hotel

--- admin/hotel_view.php
+++ admin/hotel_view.php
@@ -4,28 +4,40 @@
 
 $paramResult = checkId('id');
 
-echo $paramResult;
-
 if ($paramResult === null) {
     die('Hotel ID not found.');
 }
 
-$sql = "SELECT code
+$sql = "SELECT *
         FROM hotels
         WHERE id = $paramResult";
 $result = mysqli_query($connection, $sql);
 
 $row = $result->fetch_assoc();
 
-?>
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">Hotel Details</h4>
-                <p class="card-text">Here are the details of the selected hotel.</p>
-            </div>
+?><div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-body">
+        <!-- Flex container: left info + right buttons -->
+        <div class="d-flex justify-content-between align-items-center">
+          
+          <!-- Left side: hotel info -->
+          <div>
+            <h4 class="card-title mb-1"><?php echo $row['name']; ?></h4>
+            <p class="card-text mb-0"><?php echo $row['address']; ?></p>
+          </div>
+          
+          <!-- Right side: buttons -->
+          <div class="d-flex gap-2">
+            <a href="index.php" class="btn btn-danger">Back</a>
+            <a href="add_admin.php" class="btn btn-primary">Add Admin</a>
+          </div>
+          
         </div>
+      </div>
     </div>
+  </div>
 </div>
+
 <?php include 'include/footer.php'; ?>
