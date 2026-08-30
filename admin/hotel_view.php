fe5b5be9f194400a17138e657c1c5a98dd629e28
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-08-30 22:33:43 +0800

    link bucome a button

--- admin/hotel_view.php
+++ admin/hotel_view.php
@@ -45,14 +45,38 @@ $row = $result->fetch_assoc();
                 <div class="col-lg-8">
                     <!-- Featured blog post-->
                     <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
-                        <div class="card-body">
-                            <div class="small text-muted">January 1, 2023</div>
-                            <h2 class="card-title">Featured Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
-                        </div>
-                    </div>
+
+    <!-- Click this to enable editing -->
+    <a href="#" class="edit-card">
+        <img 
+            class="card-img-top" 
+            src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" 
+            alt="..."
+        />
+    </a>
+
+    <div class="card-body">
+
+        <div class="small text-muted">January 1, 2023</div>
+
+        <!-- These become editable -->
+        <h2 class="card-title editable-title">
+            Featured Post Title
+        </h2>
+
+        <p class="card-text editable-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Reiciendis aliquid atque, nulla? Quos cum ex quis soluta,
+            a laboriosam.
+        </p>
+
+        <!-- This changes from Read More to Save -->
+        <a href="#" class="btn btn-primary edit-button">
+            Read more →
+        </a>
+
+    </div>
+</div>
                     <!-- Nested row for non-featured blog posts-->
                     <div class="row">
                         <div class="col-lg-6">
